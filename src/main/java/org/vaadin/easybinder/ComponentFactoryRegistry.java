2c2f8086cd5d4b578c01d7ce0ff65156a0777fcd
JamesPatageule <romuald.maire@gmail.com>
2017-09-15 17:27:42 +0200

    Started the 0.0.4-SNAPSHOT
    - updated vaadin to 8.1.4
    - added "lombok" to project for code reduction
    - changed system to allow types inheritance (ex : Number -> Long,
    Integer, Float, ...)
    - Added new simples ComponentRegistry and ConverterRegistry cases
    - added simple example "Person" without any adherence (like hibernate)
    by using JSR 310 types
    - added "SimpleBuildAndBindExample" view

--- src/main/java/org/vaadin/easybinder/ComponentFactoryRegistry.java
+++ src/main/java/org/vaadin/easybinder/ComponentFactoryRegistry.java
@@ -1,35 +1,28 @@
 /*
- * Copyright 2017 Lars Sønderby Jessen
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * Copyright 2017 Lars Sønderby Jessen Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and limitations under the
+ * License.
  */
 package org.vaadin.easybinder;
 
 import java.lang.reflect.Field;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 import com.vaadin.shared.util.SharedUtil;
 import com.vaadin.ui.CheckBox;
@@ -40,71 +33,95 @@ import com.vaadin.ui.RadioButtonGroup;
 import com.vaadin.ui.TextField;
 
 public class ComponentFactoryRegistry {
-	private static ComponentFactoryRegistry instance;
+    private static ComponentFactoryRegistry instance;
 
-	protected Logger log = Logger.getLogger(getClass().getName());
+    protected Logger log = Logger.getLogger(getClass().getName());
 
-	protected Map<Class<?>, List<Pair<Predicate<Field>, Function<Field, Component>>>> builders = new HashMap<>();
+    protected Map<Class<?>, List<Pair<Predicate<Field>, Function<Field, Component>>>> builders = new HashMap<>();
 
-	protected ComponentFactoryRegistry() {
-		addBuildPattern(String.class, e -> true, e -> new TextField(SharedUtil.camelCaseToHumanFriendly(e.getName())));
-		addBuildPattern(Integer.class, e -> true, e -> new TextField(SharedUtil.camelCaseToHumanFriendly(e.getName())));
-		addBuildPattern(int.class, e -> true, e -> new TextField(SharedUtil.camelCaseToHumanFriendly(e.getName())));
-		addBuildPattern(Character.class, e -> true,
-				e -> new TextField(SharedUtil.camelCaseToHumanFriendly(e.getName())));
-		addBuildPattern(Date.class,
-				e -> Arrays.asList(e.getAnnotations()).stream().filter(f -> f instanceof Temporal)
-						.map(f -> (Temporal) f).filter(f -> f.value() == TemporalType.TIMESTAMP).findAny().isPresent(),
-				e -> new DateTimeField(SharedUtil.camelCaseToHumanFriendly(e.getName())));
-		addBuildPattern(Date.class,
-				e -> !Arrays.asList(e.getAnnotations()).stream().filter(f -> f instanceof Temporal)
-						.map(f -> (Temporal) f).filter(f -> f.value() == TemporalType.TIMESTAMP).findAny().isPresent(),
-				e -> new DateField(SharedUtil.camelCaseToHumanFriendly(e.getName())));
-		addBuildPattern(Boolean.class, 
-				e -> true,
-				e -> new CheckBox()
-				);
-		addBuildPattern(boolean.class, 
-				e -> true,
-				e -> new CheckBox()
-				);		
-	}
+    protected ComponentFactoryRegistry() {
+        addBuildPattern(String.class, e -> true, e -> new TextField(SharedUtil.camelCaseToHumanFriendly(e.getName())));
+        addBuildPattern(Character.class, e -> true,
+                e -> new TextField(SharedUtil.camelCaseToHumanFriendly(e.getName())));
 
-	public static ComponentFactoryRegistry getInstance() {
-		if (instance == null) {
-			instance = new ComponentFactoryRegistry();
-		}
+        addBuildPattern(Number.class, e -> true, e -> new TextField(SharedUtil.camelCaseToHumanFriendly(e.getName())));
 
-		return instance;
-	}
+        addBuildPattern(int.class, e -> true, e -> new TextField(SharedUtil.camelCaseToHumanFriendly(e.getName())));
 
-	public void addBuildPattern(Class<?> propertyType, Predicate<Field> matcher,
-			Function<Field, Component> componentFactory) {
-		List<Pair<Predicate<Field>, Function<Field, Component>>> lst = builders.get(propertyType);
-		if (lst == null) {
-			lst = new LinkedList<>();
-			builders.put(propertyType, lst);
-		}
-		lst.add(new Pair<>(matcher, componentFactory));
-	}
+        addBuildPattern(Date.class, e -> true,
+                // e -> Arrays.asList(e.getAnnotations()).stream().filter(f -> f instanceof Temporal)
+                // .map(f -> (Temporal) f).filter(f -> f.value() == TemporalType.TIMESTAMP).findAny().isPresent(),
+                e -> new DateTimeField(SharedUtil.camelCaseToHumanFriendly(e.getName())));
 
-	public Optional<Component> createComponent(Field field) {
-		if (!builders.containsKey(field.getType())) {
-			if (Enum.class.isAssignableFrom(field.getType())) {
-				return Optional.of(createGenericEnumComponent(field));
-			}
-			log.log(Level.INFO, "No build rule for field=<{0}> with type=<{1}>",
-					new Object[] { field, field.getType() });
-			return Optional.empty();
-		}
+        addBuildPattern(LocalDate.class, e -> true,
+                e -> new DateField(SharedUtil.camelCaseToHumanFriendly(e.getName())));
+        addBuildPattern(LocalDateTime.class, e -> true,
+                e -> new DateTimeField(SharedUtil.camelCaseToHumanFriendly(e.getName())));
 
-		return Optional.ofNullable(builders.get(field.getType()).stream().filter(e -> e.getFirst().test(field))
-				.findFirst().get().getSecond().apply(field));
-	}
+        addBuildPattern(Boolean.class, e -> true,
+                e -> new CheckBox(SharedUtil.camelCaseToHumanFriendly(e.getName()), false));
+        addBuildPattern(boolean.class, e -> true,
+                e -> new CheckBox(SharedUtil.camelCaseToHumanFriendly(e.getName()), false));
+    }
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	protected Component createGenericEnumComponent(Field field) {
-		return new RadioButtonGroup(SharedUtil.camelCaseToHumanFriendly(field.getName()),
-				Arrays.asList(field.getType().getEnumConstants()));
-	}
+    public static ComponentFactoryRegistry getInstance() {
+        if (instance == null)
+            instance = new ComponentFactoryRegistry();
+
+        return instance;
+    }
+
+    public void addBuildPattern(Class<?> propertyType, Predicate<Field> matcher,
+            Function<Field, Component> componentFactory) {
+        List<Pair<Predicate<Field>, Function<Field, Component>>> lst = builders.get(propertyType);
+        if (lst == null) {
+            lst = new LinkedList<>();
+            builders.put(propertyType, lst);
+        }
+        lst.add(new Pair<>(matcher, componentFactory));
+    }
+
+    public Optional<Component> createComponent(Field field) {
+
+        Optional<Component> cpt = null;
+
+        for (Entry<Class<?>, List<Pair<Predicate<Field>, Function<Field, Component>>>> builder : builders.entrySet())
+            if (builder.getKey().isAssignableFrom(field.getType())) {
+                Optional<Pair<Predicate<Field>, Function<Field, Component>>> o = builder.getValue().stream()
+                        .filter(e -> e.getFirst().test(field)).findFirst();
+                if (o.isPresent()) {
+                    cpt = Optional.ofNullable(o.get().getSecond().apply(field));
+                    break;
+                }
+            }
+
+        if (cpt == null)
+            if (Enum.class.isAssignableFrom(field.getType()))
+                cpt = Optional.of(createGenericEnumComponent(field));
+            else {
+                log.log(Level.INFO, "No build rule for field=<{0}> with type=<{1}>",
+                        new Object[] { field, field.getType() });
+                cpt = Optional.empty();
+            }
+
+        return cpt;
+
+        // if (!builders.containsKey(field.getType())) {
+        // if (Enum.class.isAssignableFrom(field.getType())) {
+        // return Optional.of(createGenericEnumComponent(field));
+        // }
+        // log.log(Level.INFO, "No build rule for field=<{0}> with type=<{1}>",
+        // new Object[] { field, field.getType() });
+        // return Optional.empty();
+        // }
+        //
+        // return Optional.ofNullable(builders.get(field.getType()).stream().filter(e -> e.getFirst().test(field))
+        // .findFirst().get().getSecond().apply(field));
+    }
+
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    protected Component createGenericEnumComponent(Field field) {
+        return new RadioButtonGroup(SharedUtil.camelCaseToHumanFriendly(field.getName()),
+                Arrays.asList(field.getType().getEnumConstants()));
+    }
 }
--- src/main/java/org/vaadin/easybinder/ConverterRegistry.java
+++ src/main/java/org/vaadin/easybinder/ConverterRegistry.java
@@ -1,17 +1,10 @@
 /*
- * Copyright 2017 Lars Sønderby Jessen
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * Copyright 2017 Lars Sønderby Jessen Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and limitations under the
+ * License.
  */
 package org.vaadin.easybinder;
 
@@ -26,57 +19,88 @@ import com.vaadin.data.Converter;
 import com.vaadin.data.Result;
 import com.vaadin.data.converter.LocalDateTimeToDateConverter;
 import com.vaadin.data.converter.LocalDateToDateConverter;
+import com.vaadin.data.converter.StringToFloatConverter;
 import com.vaadin.data.converter.StringToIntegerConverter;
+import com.vaadin.data.converter.StringToLongConverter;
 
 public class ConverterRegistry {
-	static ConverterRegistry instance;
+    static ConverterRegistry instance;
 
-	protected Map<Pair<Class<?>, Class<?>>, Converter<?, ?>> converters = new HashMap<>();
+    protected Map<Pair<Class<?>, Class<?>>, Converter<?, ?>> converters = new HashMap<>();
 
-	public static ConverterRegistry getInstance() {
-		if (instance == null) {
-			instance = new ConverterRegistry();
-		}
-		return instance;
-	}
+    public static ConverterRegistry getInstance() {
+        if (instance == null) {
+            instance = new ConverterRegistry();
+        }
+        return instance;
+    }
 
-	private ConverterRegistry() {
-		Converter<String, Integer> c = Converter.from(e -> {
-			if (e.length() == 0) {
-				return Result.error("Must be a number");
-			}
-			try {
-				return Result.ok(Integer.parseInt(e));
-			} catch (NumberFormatException ex) {
-				return Result.error("Must be a number");
-			}
-		}, e -> Integer.toString(e));
+    private ConverterRegistry() {
+        Converter<String, Integer> c1 = Converter.from(e -> {
+            if (e.length() == 0)
+                return Result.error("Must be a number");
+            try {
+                return Result.ok(Integer.parseInt(e));
+            } catch (NumberFormatException ex) {
+                return Result.error("Must be a number");
+            }
+        }, e -> Integer.toString(e));
 
-		registerConverter(String.class, int.class, c);
-		registerConverter(String.class, Integer.class,
-				new NullConverter<String>("").chain(new StringToIntegerConverter("Conversion failed")));
-		registerConverter(String.class, Character.class,
-				Converter.from(
-						e -> e.length() == 0 ? Result.ok(null)
-								: (e.length() == 1 ? Result.ok(e.charAt(0)) : Result.error("Must be 1 character")),
-						f -> f == null ? "" : "" + f));
-		registerConverter(LocalDateTime.class, Date.class, new LocalDateTimeToDateConverter(ZoneId.systemDefault()));
-		registerConverter(LocalDate.class, Date.class, new LocalDateToDateConverter());
-	}
+        registerConverter(String.class, int.class, c1);
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public <PRESENTATIONTYPE, MODELTYPE> Converter<PRESENTATIONTYPE, MODELTYPE> getConverter(
-			Class<PRESENTATIONTYPE> presentationType, Class<MODELTYPE> modelType) {
-		return (Converter) converters.get(new Pair<>(presentationType, modelType));
-	}
+        Converter<String, Long> c2 = Converter.from(e -> {
+            if (e.length() == 0)
+                return Result.error("Must be a number");
+            try {
+                return Result.ok(Long.parseLong(e));
+            } catch (NumberFormatException ex) {
+                return Result.error("Must be a number");
+            }
+        }, e -> Long.toString(e));
 
-	public <PRESENTATIONTYPE, MODELTYPE> void registerConverter(Class<PRESENTATIONTYPE> presentationType,
-			Class<MODELTYPE> modelType, Converter<PRESENTATIONTYPE, MODELTYPE> converter) {
-		converters.put(new Pair<>(presentationType, modelType), converter);
-	}
+        registerConverter(String.class, long.class, c2);
 
-	public <PRESENTATIONTYPE, MODELTYPE> void unregisterConverter(Class<PRESENTATIONTYPE> presentationType,
-			Class<MODELTYPE> modelType) {
-		converters.remove(new Pair<>(presentationType, modelType));
-	}
+        Converter<String, Float> c3 = Converter.from(e -> {
+            if (e.length() == 0)
+                return Result.error("Must be a number");
+            try {
+                return Result.ok(Float.parseFloat(e));
+            } catch (NumberFormatException ex) {
+                return Result.error("Must be a number");
+            }
+        }, e -> Float.toString(e));
+
+        registerConverter(String.class, float.class, c3);
+
+        registerConverter(String.class, Integer.class,
+                new NullConverter<String>("").chain(new StringToIntegerConverter("Conversion failed")));
+        registerConverter(String.class, Long.class,
+                new NullConverter<String>("").chain(new StringToLongConverter("Conversion failed")));
+        registerConverter(String.class, Float.class,
+                new NullConverter<String>("").chain(new StringToFloatConverter("Conversion failed")));
+
+        registerConverter(String.class, Character.class,
+                Converter.from(
+                        e -> e.length() == 0 ? Result.ok(null)
+                                : (e.length() == 1 ? Result.ok(e.charAt(0)) : Result.error("Must be 1 character")),
+                        f -> f == null ? "" : "" + f));
+        registerConverter(LocalDateTime.class, Date.class, new LocalDateTimeToDateConverter(ZoneId.systemDefault()));
+        registerConverter(LocalDate.class, Date.class, new LocalDateToDateConverter());
+    }
+
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    public <PRESENTATIONTYPE, MODELTYPE> Converter<PRESENTATIONTYPE, MODELTYPE> getConverter(
+            Class<PRESENTATIONTYPE> presentationType, Class<MODELTYPE> modelType) {
+        return (Converter) converters.get(new Pair<>(presentationType, modelType));
+    }
+
+    public <PRESENTATIONTYPE, MODELTYPE> void registerConverter(Class<PRESENTATIONTYPE> presentationType,
+            Class<MODELTYPE> modelType, Converter<PRESENTATIONTYPE, MODELTYPE> converter) {
+        converters.put(new Pair<>(presentationType, modelType), converter);
+    }
+
+    public <PRESENTATIONTYPE, MODELTYPE> void unregisterConverter(Class<PRESENTATIONTYPE> presentationType,
+            Class<MODELTYPE> modelType) {
+        converters.remove(new Pair<>(presentationType, modelType));
+    }
 }
